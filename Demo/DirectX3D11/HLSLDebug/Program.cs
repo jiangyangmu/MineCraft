1c481723ebd3f3f8e8a2b11506e2b1c38256626a
Yangmu Jiang <celsius.j@gmail.com>
2019-12-08 16:00:28 -0800

    Add shader disassembly.

---
+++ Demo/DirectX3D11/HLSLDebug/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SharpDX;
+using SharpDX.D3DCompiler;
+
+namespace HLSLDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vertexShaderByteCode = ShaderBytecode.CompileFromFile("Shader.hlsl", "VS", "vs_4_0");
+            var vertexShaderDisassembly = new ShaderBytecode(vertexShaderByteCode).Disassemble();
+            Console.Write("---- Vertex Shader Disassembly ----\r\n" + vertexShaderDisassembly);
+        }
+    }
+}
